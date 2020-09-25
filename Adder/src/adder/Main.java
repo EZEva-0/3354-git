c3300d9dc886d0c54f8ac922de0ffd336635f028
EZEva-0 <evanbrancowork@gmail.com>
2020-09-24 22:00:35 -0500

    Modified Main.java
    Rewrote addArguments method, it now accepts arbitray number of input integers

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,17 +1,24 @@
 package adder;
 
-public class Main {
-
-    public static void main(String[] args) {
-        try {
-            int result = addArguments(args);
-            System.out.println(result);
-        } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
-        }
-    }
-
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
-    }
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+        	try 	
+		{
+            		int result = addArguments(args);
+            		System.out.println(result);
+       		} 
+		catch (Exception e) 
+		{
+            		System.err.println("Please provide two integers to add");
+        	}
+    	}
+    	private static int addArguments(String[] args) 
+	{
+		int temp = 0;
+		for(int i=0; i< args.length; i++)
+			temp += Integer.valueOf(args[i]);
+        	return temp;
+    	}
 }
